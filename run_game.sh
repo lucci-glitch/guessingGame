66caf905fe746e10dec87d80148960437041ca89
lucci-glitch <72732913+lucci-glitch@users.noreply.github.com>
2020-10-16 09:58:49 +0200

    Added a bash script to run the game

--- run_game.sh
+++ run_game.sh
@@ -1,2 +1,18 @@
-#!/bin/bash/
+#! /bin/bash
+
+NAME="Lukas_Blomdell"
+SUFFIX="_labb"
+
+echo "$NAME program"
+mkdir ../"$NAME$SUFFIX"
+cp -r ./*.java ../"$NAME$SUFFIX"
+cd ../"$NAME$SUFFIX"
+echo "Running game from $PWD"
+echo "compiling..."
+javac GuessingGame.java
+echo "running..."
+java GuessingGame
+echo "Done"
+rm -r *.class
+ls
 
